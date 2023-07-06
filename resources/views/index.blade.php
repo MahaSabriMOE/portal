2ecf18efdcbafe7606c7143d26fe0ff5c36537e7
MahaSabriMOE <maha.sabri@moe.edu.ps>
2023-07-06 13:46:50 +0300

    www

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,27 @@
 @section('content')
 
 
-<h1>This is Index page</h1>
 <!--<img src="/images_folder/cover.jpg" class="img-fluid" alt="">-->
 <!--Carousel-->
-<header>
-  <h1>This is header section</h1>
-  <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
+
+  
+  <!-- Hero -->
+<div class="p-5 text-center bg-image rounded-3" style="
+background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+height: 400px;
+">
+
+<div class="d-flex justify-content-center align-items-center h-100">
+  <div class="text-white">
+    <h1 class="mb-3">ترويسة </h1>
+    <h4 class="mb-3">This is Index page</h4>
+    <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
   </div>
 </div>
-</header>
+</div>
+</div>
+<!-- Hero -->
+
   
   
   <!-- Counters-->

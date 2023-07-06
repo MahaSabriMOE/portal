a0cd448c6d206bb6b5c41eca841bc74b175b871b
MahaSabriMOE <maha.sabri@moe.edu.ps>
2023-07-06 13:37:14 +0300

    kk

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,19 @@
 <!--Carousel-->
 <header>
   <h1>This is header section</h1>
-  
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
 </header>
   
   
